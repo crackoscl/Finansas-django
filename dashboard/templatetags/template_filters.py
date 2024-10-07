ec20285f6ae346f6410b67955023fa89b2a16883
crackoscl <gilbert.ata@gmail.com>
2024-10-06 23:18:26 -0300

    se agrega custom filter para fomateo de valores

--- dashboard/templatetags/template_filters.py
+++ dashboard/templatetags/template_filters.py
@@ -5,3 +5,12 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.filter
+def format_amount(value):
+    try:
+        amount_int = int(value)
+        return f"${amount_int:,}".replace(',', '.')
+    except (ValueError, TypeError):
+        return value  # Retorna el valor original si no se puede convertir
